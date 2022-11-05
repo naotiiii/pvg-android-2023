c8fbb5aa50d9e33c3144977b5f730d5040eba06f
naoto kojima <>
2022-11-05 19:29:12 +0900

    スプラッシュ設定

---
+++ pvg2023/app/src/main/java/com/pvg/pvg2023/ui/splash/SplashActivity.kt
@@ -0,0 +1,37 @@
+package com.pvg.pvg2023.ui.splash
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+import com.pvg.pvg2023.MainActivity
+import com.pvg.pvg2023.databinding.ActivitySplashBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+/**
+ * スプラッシュ画面
+ */
+class SplashActivity: AppCompatActivity() {
+
+    private lateinit var binding: ActivitySplashBinding
+
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        CoroutineScope(Dispatchers.Main).launch {
+            delay(2000)
+            val intent = Intent(this@SplashActivity, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+}
