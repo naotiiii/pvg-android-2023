30cb86f1f7558438fdb89d6d1723251ce1bb1fb4
naoto kojima <>
2022-12-08 01:14:47 +0900

    カレンダー表示する画面のレイアウト作成

--- pvg2023/app/src/main/java/com/pvg/pvg2023/ui/review/ReviewFragment.kt
+++ pvg2023/app/src/main/java/com/pvg/pvg2023/ui/review/ReviewFragment.kt
@@ -1,19 +1,29 @@
 package com.pvg.pvg2023.ui.review
 
+import android.R
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.applandeo.materialcalendarview.CalendarView
+import com.applandeo.materialcalendarview.EventDay
 import com.pvg.pvg2023.databinding.FragmentReviewBinding
+import java.util.*
+import kotlin.collections.ArrayList
 
+
+/**
+ * カレンダー Fragment
+ */
 class ReviewFragment : Fragment() {
 
     private var _binding: FragmentReviewBinding? = null
 
-    // This property is only valid between onCreateView and
-    // onDestroyView.
+    private lateinit var calendarView: CalendarView
+
     private val binding get() = _binding!!
 
     override fun onCreateView(
@@ -27,11 +37,27 @@ class ReviewFragment : Fragment() {
         _binding = FragmentReviewBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
+        setImageOfDay()
         return root
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    /**
+     * カレンダー日付に画像設定
+     */
+    fun setImageOfDay() {
+        calendarView = binding.calendarView
+        val calender = Calendar.getInstance()
+        calender.set(2022, 12, 10)
+
+        val events: MutableList<EventDay> = ArrayList()
+
+        events.add(EventDay(calender, R.drawable.ic_delete))
+
+        calendarView.setEvents(events)
     }
 }
